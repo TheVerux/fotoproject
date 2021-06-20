d37ee1668556d6786601629cd81097495629e9b8
Raphael Gosteli <rgo112011@iet-gibb.ch>
2021-06-20 17:55:56 +0200

    add embed for all pdfs

--- src/app/portfolio-files/portfolio-files.component.ts
+++ src/app/portfolio-files/portfolio-files.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {DomSanitizer, SafeResourceUrl} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-portfolio-files',
@@ -10,10 +11,15 @@ export class PortfolioFilesComponent implements OnInit {
   @Input()
   name!: string;
 
-  constructor() {
+  constructor(private sanitizer: DomSanitizer) {
   }
 
   ngOnInit(): void {
+  }
+
+  getFileUrl(fileName: string): SafeResourceUrl {
+    const url = `/assets/doc/${this.name}/${fileName}.pdf`;
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
   }
 
 }
